d3b450fdd84977c750a75e6af655eef0ac21271b
Abhijeet98s <abhi.21smiley00@gmail.com>
2022-11-25 19:51:49 +0530

    week-4 task added

---
+++ Week-04/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+// writing file
+fs.writeFileSync('info.txt','Hello folks, My name is Abhijeet Sharma')
+
+
+// updating file
+// fs.appendFileSync('info.txt','\n\nI am learning web dev from Crework.')
+
+
+// reading file
+// const data = fs.readFileSync('info.txt',{encoding:'utf8'})
+// console.log(data);
+
+
+//renaming file
+// fs.renameSync("info.txt","data.txt")
+
+
+// deleting file
+// fs.unlinkSync("data.txt")
+
+
+
